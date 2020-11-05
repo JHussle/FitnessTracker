917712d0ddd3d77c7930387a1f0b0071fbe8cce6
JSmith <65369149+JHussle@users.noreply.github.com>
2020-11-04 18:30:06 -0600

    Initial Commit

---
+++ FitnessTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessTracker
+//
+//  Created by Joey Smith on 11/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
